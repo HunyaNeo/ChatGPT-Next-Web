7a3db5015b246f0b83a53cca6e32fde4ec9a32fd
HunyaNeo <118657067+HunyaNeo@users.noreply.github.com>
2023-06-30 14:59:31 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT Next
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          hunya is so handsome, I liked him so much.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
